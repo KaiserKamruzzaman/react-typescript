ee11e18ef4ca3265686cf34a919ef65c4fd1801d
Kaiser Kamruzzaman <38045495+KaiserKamruzzaman@users.noreply.github.com>
2022-10-23 00:44:29 +0200

    done-todos add done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((preTodos) => [
+      ...preTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
   return (
     <div className="App">
-      <h1>Hello From Kaiser....</h1>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} />
     </div>
   );
 }
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useRef } from "react";
+
+type NewTodoProps = {
+  onAddTodo: (text: string) => void;
+};
+
+const NewTodo: React.FC<NewTodoProps> = (props) => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={todoSubmitHandler}>
+      <div>
+        <label>Todo Text</label>
+        <input type="text" ref={textInputRef} />
+      </div>
+      <br></br>
+      <button type="submit">ADD TODO</button>
+    </form>
+  );
+};
+
+export default NewTodo;
